19bb9671bdbed1a0ede43a4d6f01b8c458279ff3
PUSHRSP <pushrsp@gmail.com>
2023-03-04 17:00:03 +0900

    배치 쓰기 위한 애노테이션 추가
    @EnableBatchProcessing

--- src/main/java/com/example/springbatchpractice/SpringBatchPracticeApplication.java
+++ src/main/java/com/example/springbatchpractice/SpringBatchPracticeApplication.java
@@ -1,9 +1,11 @@
 package com.example.springbatchpractice;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchPracticeApplication {
 
     public static void main(String[] args) {
